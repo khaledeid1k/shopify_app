faee9bf1a0ee0daa16003b1939d478287f6c33b2
khaledeid1k <khaled.eid1k1k@gmail.com>
2023-11-17 20:34:11 +0200

    Initial commit

---
+++ app/src/main/java/com/kh/mo/shopyapp/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.kh.mo.shopyapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.kh.mo.shopyapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var controller: NavController
+    private lateinit var navHostFragment: NavHostFragment
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding  = DataBindingUtil.setContentView(
+            this,R.layout.activity_main
+        )
+        setUpBottomNavigationView()
+    }
+
+    private fun setUpBottomNavigationView() {
+        navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.fragment_container) as NavHostFragment
+        controller = navHostFragment.navController
+        NavigationUI.setupWithNavController(binding.bottomNavigation, controller)
+
+    }
+}
