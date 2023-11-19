f80676110857e250c61b1108882485bea4982fa3
khaledeid1k <khaled.eid1k1k@gmail.com>
2023-11-19 18:02:15 +0200

    make product screen

--- app/src/main/java/com/kh/mo/shopyapp/ui/product/ProductInfoFragment.kt
+++ app/src/main/java/com/kh/mo/shopyapp/ui/product/ProductInfoFragment.kt
@@ -1,4 +1,4 @@
-package com.kh.mo.shopyapp.ui
+package com.kh.mo.shopyapp.ui.product
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,15 +7,16 @@ import android.view.View
 import android.view.ViewGroup
 import com.kh.mo.shopyapp.R
 
-class ProductFragment : Fragment() {
+
+class ProductInfoFragment : Fragment() {
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_product, container, false)
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_product_info, container, false)
     }
-
 
 }
