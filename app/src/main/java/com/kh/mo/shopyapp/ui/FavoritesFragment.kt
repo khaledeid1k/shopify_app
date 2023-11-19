fa3a54a5449f6a4368d549c445854d1c9fed5ada
khaledeid1k <khaled.eid1k1k@gmail.com>
2023-11-19 08:45:56 +0200

    make favorite screen

---
+++ app/src/main/java/com/kh/mo/shopyapp/ui/FavoritesFragment.kt
@@ -0,0 +1,22 @@
+package com.kh.mo.shopyapp.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.kh.mo.shopyapp.R
+
+
+class FavoritesFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_favorites, container, false)
+    }
+
+
+}
