32389aebb545c081729927b0a6c557713d00d865
Hanaa Hany <hanaahany822@gmail.com>
2023-11-19 22:15:01 +0200

    create model ui , response

---
+++ app/src/main/java/com/kh/mo/shopyapp/model/response/barnds/RuleResponse.kt
@@ -0,0 +1,13 @@
+package com.kh.mo.shopyapp.model.response.barnds
+
+
+import com.google.gson.annotations.SerializedName
+
+data class RuleResponse(
+    @SerializedName("column")
+    val column: String?,
+    @SerializedName("condition")
+    val condition: String?,
+    @SerializedName("relation")
+    val relation: String?
+)
