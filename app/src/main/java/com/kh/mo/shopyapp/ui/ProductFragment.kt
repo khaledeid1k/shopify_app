43196e8fac4a38dec67e35c3e8ca2eac3b766f37
khaledeid1k <khaled.eid1k1k@gmail.com>
2023-11-19 09:29:55 +0200

    start product screen

---
+++ app/src/main/java/com/kh/mo/shopyapp/ui/ProductFragment.kt
@@ -0,0 +1,21 @@
+package com.kh.mo.shopyapp.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.kh.mo.shopyapp.R
+
+class ProductFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_product, container, false)
+    }
+
+
+}
