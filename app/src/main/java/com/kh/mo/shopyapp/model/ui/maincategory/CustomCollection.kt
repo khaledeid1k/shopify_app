f3cd36fb20e67dcfc5a12a0ed6ba9c9a53d37da6
Hanaa Hany <hanaahany822@gmail.com>
2023-11-20 10:08:59 +0200

    create model of main category

---
+++ app/src/main/java/com/kh/mo/shopyapp/model/ui/maincategory/CustomCollection.kt
@@ -0,0 +1,11 @@
+package com.kh.mo.shopyapp.model.ui.maincategory
+
+
+import com.google.gson.annotations.SerializedName
+import com.kh.mo.shopyapp.model.response.maincategory.ImageResponse
+
+data class CustomCollection(
+    @SerializedName("image")
+    val image: ImageResponse?,
+    val title: String?
+    )
