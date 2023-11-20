939829e5b970e35af151c53a5af6e0daa2891c6a
Moaaz AbdEl-Salam <moaazabdelsalam99@gmail.com>
2023-11-20 15:40:38 +0200

    added getDiscountCode fun to service and

--- app/src/main/java/com/kh/mo/shopyapp/remote/service/Services.kt
+++ app/src/main/java/com/kh/mo/shopyapp/remote/service/Services.kt
@@ -1,9 +1,11 @@
 package com.kh.mo.shopyapp.remote.service
 
+import com.kh.mo.shopyapp.model.response.ads.DiscountCodeResponse
 import com.kh.mo.shopyapp.model.response.barnds.BrandsResponse
 import com.kh.mo.shopyapp.model.response.maincategory.MainCategoryResponse
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface Services {
 
@@ -13,6 +15,10 @@ interface Services {
     @GET("custom_collections.json")
     suspend fun getMainCategories(): Response<MainCategoryResponse>
 
-
+    @GET("price_rules/{priceRuleId}/discount_codes/{discountCodeId}.json")
+    suspend fun getDiscountCode(
+        @Path("priceRuleId") priceRuleId: String,
+        @Path("discountCodeId") discountCodeId: String
+    ): DiscountCodeResponse
 
 }
