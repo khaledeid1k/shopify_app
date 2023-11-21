24ee725191fca74316a7550bcfdda3e75dbe39a4
khaledeid1k <khaled.eid1k1k@gmail.com>
2023-11-21 16:41:46 +0200

    add product response model and make parcelable

--- app/src/main/java/com/kh/mo/shopyapp/model/response/product/ProductDetails.kt
+++ app/src/main/java/com/kh/mo/shopyapp/model/response/product/ProductDetails.kt
@@ -1,6 +1,11 @@
 package com.kh.mo.shopyapp.model.response.product
 
-data class ProductX(
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+
+@Parcelize
+data class ProductDetails(
     val admin_graphql_api_id: String?,
     val body_html: String?,
     val created_at: String?,
@@ -14,9 +19,9 @@ data class ProductX(
     val published_scope: String?,
     val status: String?,
     val tags: String?,
-    val template_suffix: Any?,
+    val template_suffix: String?,
     val title: String?,
     val updated_at: String?,
     val variants: List<Variant?>?,
     val vendor: String?
-)
+) : Parcelable
---
+++ app/src/main/java/com/kh/mo/shopyapp/model/response/product/ProductResponse.kt
@@ -0,0 +1,5 @@
+package com.kh.mo.shopyapp.model.response.product
+
+data class ProductResponse(
+    val productDetails: ProductDetails?
+)
