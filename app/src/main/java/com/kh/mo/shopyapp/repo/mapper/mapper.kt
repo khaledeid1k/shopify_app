346aaa9eadbff31a2fd9c1a7abff6031d3725ec5
khaledeid1k <khaled.eid1k1k@gmail.com>
2023-11-21 03:49:18 +0200

    make map to login response

--- app/src/main/java/com/kh/mo/shopyapp/repo/mapper/mapper.kt
+++ app/src/main/java/com/kh/mo/shopyapp/repo/mapper/mapper.kt
@@ -4,17 +4,27 @@ import com.kh.mo.shopyapp.model.entity.CustomerEntity
 import com.kh.mo.shopyapp.model.request.CustomerDataRequest
 import com.kh.mo.shopyapp.model.request.UserData
 import com.kh.mo.shopyapp.model.response.create_customer.CustomerResponse
+import com.kh.mo.shopyapp.model.response.login.Login
 
 fun CustomerResponse.convertCustomerResponseToCustomerEntity(): CustomerEntity {
     return CustomerEntity(
         customer?.id!!,
         customer.first_name!!,
-    customer.email!!,true
+        customer.email!!, true
     )
 }
 
 
 fun UserData.convertUserDataToCustomerData(): CustomerDataRequest {
     return CustomerDataRequest(
-       userName,email,true,password,password)
+        userName, email, true, password, password
+    )
+}
+
+fun Login.convertLoginToUserData(): UserData {
+    return this.customers?.get(0).let {
+        UserData(id = it?.id!!, it.first_name.toString(), it.email.toString())
+    }
+
+
 }
