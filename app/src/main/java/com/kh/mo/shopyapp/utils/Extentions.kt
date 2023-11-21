d1b5471e45c336c14e2b229cfb306db9d754bd12
Moaaz AbdEl-Salam <moaazabdelsalam99@gmail.com>
2023-11-21 20:41:44 +0200

    add currency conversion extension functions

--- app/src/main/java/com/kh/mo/shopyapp/utils/Extentions.kt
+++ app/src/main/java/com/kh/mo/shopyapp/utils/Extentions.kt
@@ -3,9 +3,12 @@ package com.kh.mo.shopyapp.utils
 import android.text.Editable
 import android.text.TextWatcher
 import com.google.android.material.textfield.TextInputEditText
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 
 
-fun TextInputEditText.getText(textValue:(text:String)->Unit) {
+fun TextInputEditText.getText(textValue: (text: String) -> Unit) {
     this.addTextChangedListener(object : TextWatcher {
         override fun beforeTextChanged(
             charSequence: CharSequence,
@@ -27,5 +30,22 @@ fun TextInputEditText.getText(textValue:(text:String)->Unit) {
 
         override fun afterTextChanged(editable: Editable) {}
     })
+}
 
+fun Double.toEGP(toEGPRate: Double): Double {
+    return this * toEGPRate
+}
+
+fun Double.toGBP(toGBPRate: Double): Double {
+    return this * toGBPRate
+}
+
+fun Double.toEUR(toEURRate: Double): Double {
+    return this * toEURRate
+}
+
+fun main() {
+    val sdf = SimpleDateFormat("EEEMMMddyyyy", Locale.ENGLISH)
+    val calendar = Calendar.getInstance()
+    println(sdf.format(calendar.time) == "TueNov212023")
 }
