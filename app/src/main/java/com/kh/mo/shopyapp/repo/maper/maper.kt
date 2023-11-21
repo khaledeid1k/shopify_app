c198e11108a49a58047316a23d98010331aa3ae9
Hanaa Hany <hanaahany822@gmail.com>
2023-11-21 20:42:32 +0200

    create view model for sub category

--- app/src/main/java/com/kh/mo/shopyapp/repo/maper/maper.kt
+++ app/src/main/java/com/kh/mo/shopyapp/repo/maper/maper.kt
@@ -1,9 +1,12 @@
 package com.kh.mo.shopyapp.repo.maper
 
+import com.kh.mo.shopyapp.model.response.allproducts.AllProductsResponse
 import com.kh.mo.shopyapp.model.response.barnds.BrandsResponse
 import com.kh.mo.shopyapp.model.response.barnds.SmartCollection
 import com.kh.mo.shopyapp.model.response.maincategory.MainCategoryResponse
+import com.kh.mo.shopyapp.model.response.productsofbrand.ProductResponse
 import com.kh.mo.shopyapp.model.response.productsofbrand.ProductsOfSpecificBrandResponse
+import com.kh.mo.shopyapp.model.ui.allproducts.Products
 import com.kh.mo.shopyapp.model.ui.maincategory.CustomCollection
 import com.kh.mo.shopyapp.model.ui.productsofbrand.Product
 
@@ -30,6 +33,14 @@ fun ProductsOfSpecificBrandResponse.convertToProduct():List<Product>{
     return this.products?.map {
 
         Product(it?.images,it?.title,it?.variants)
+    } ?: emptyList()
+
+}
+fun AllProductsResponse.convertToAllProducts():List<Products>{
+
+    return this.products?.map {
+
+        Products(it?.productType)
     } ?: emptyList()
 
 }
--- app/src/main/java/com/kh/mo/shopyapp/ui/base/BaseViewModelFactory.kt
+++ app/src/main/java/com/kh/mo/shopyapp/ui/base/BaseViewModelFactory.kt
@@ -3,6 +3,7 @@ package com.kh.mo.shopyapp.ui.base
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.kh.mo.shopyapp.repo.Repo
+import com.kh.mo.shopyapp.ui.category.viewmodel.CategoryViewModel
 import com.kh.mo.shopyapp.ui.home.viewmodel.HomeViewModel
 
 class BaseViewModelFactory(private val repo: Repo) : ViewModelProvider.Factory {
@@ -10,6 +11,8 @@ class BaseViewModelFactory(private val repo: Repo) : ViewModelProvider.Factory {
         return when (modelClass) {
            // SettingsViewModel::class.java -> SettingsViewModel(repo) as T
             HomeViewModel::class.java->HomeViewModel(repo) as T
+            CategoryViewModel::class.java->CategoryViewModel(repo) as T
+
             else -> throw Throwable("Unsupported view model")
         }
     }
