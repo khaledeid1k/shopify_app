d4220ed64785118e3e4aed5a6eb273c136c91f86
khaledeid1k <khaled.eid1k1k@gmail.com>
2023-11-21 23:22:06 +0200

    rearrange in packages

--- app/src/main/java/com/kh/mo/shopyapp/ui/product/product_Info/view/ProductInfoFragment.kt
+++ app/src/main/java/com/kh/mo/shopyapp/ui/product/product_Info/view/ProductInfoFragment.kt
@@ -1,4 +1,4 @@
-package com.kh.mo.shopyapp.ui.product.view
+package com.kh.mo.shopyapp.ui.product.product_Info.view
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -8,14 +8,15 @@ import android.view.ViewGroup
 import com.kh.mo.shopyapp.R
 
 
-class DetailsFragment : Fragment() {
+class ProductInfoFragment : Fragment() {
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_details, container, false)
+        return inflater.inflate(R.layout.fragment_product_info, container, false)
     }
 
 }
--- app/src/main/java/com/kh/mo/shopyapp/ui/product/product_details/view/DetailsFragment.kt
+++ app/src/main/java/com/kh/mo/shopyapp/ui/product/product_details/view/DetailsFragment.kt
@@ -1,4 +1,4 @@
-package com.kh.mo.shopyapp.ui.product.view
+package com.kh.mo.shopyapp.ui.product.product_details.view
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -8,15 +8,14 @@ import android.view.ViewGroup
 import com.kh.mo.shopyapp.R
 
 
-class ReviewsFragment : Fragment() {
-
+class DetailsFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_reviews, container, false)
+        return inflater.inflate(R.layout.fragment_details, container, false)
     }
 
 }
