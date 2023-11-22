9a10e282457e3ae8ad378d73f149aada5bdab576
Hanaa Hany <hanaahany822@gmail.com>
2023-11-22 12:52:39 +0200

    update item product

--- app/src/main/java/com/kh/mo/shopyapp/model/response/productsofbrand/ProductsOfSpecificBrandResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.kh.mo.shopyapp.model.response.productsofbrand
-
-
-import com.google.gson.annotations.SerializedName
-
-data class ProductsOfSpecificBrandResponse(
-    @SerializedName("products")
-    val products: List<ProductResponse?>?
-)
